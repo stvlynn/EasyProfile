1f2d71ecf361a9b0d68c6739e7cfc0235bed1b69
Steven Lynn <i@stv.lol>
2025-02-28 13:26:59 +0800

    fix: update default Twitter user bio text

--- src/components/cards/TwitterCard.tsx
+++ src/components/cards/TwitterCard.tsx
@@ -36,7 +36,7 @@ export const TwitterCard: React.FC<TwitterCardProps> = ({ card }) => {
       },
       'default': {
         name: 'Twitter User',
-        bio: 'Twitter should be public...',
+        bio: 'Twitter bio default text',
         avatar: 'https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png'
       }
     };
